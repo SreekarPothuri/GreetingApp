880044e21546bdf40517cd67c74b013f36feae60
Your Name <you@example.com>
2020-12-21 00:49:39 +0530

    RFefactored: code to find greeting by id

--- src/main/java/com/blz/greeting/controller/GreetingController.java
+++ src/main/java/com/blz/greeting/controller/GreetingController.java
@@ -1,7 +1,11 @@
 package com.blz.greeting.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +17,26 @@ import com.blz.greeting.service.IGreetingService;
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
-	
-	@Autowired
-    private IGreetingService iGreetingService;
 
-    @GetMapping(value = {"/home"})
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-    	User user = new User();
-        user.setFirstName(name);
-        return iGreetingService.addGreeting(user);
-    }
+	@Autowired
+	private IGreetingService iGreetingService;
+
+	User user = new User();
+
+	@GetMapping(value = { "/home" })
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		user.setFirstName(name);
+		return iGreetingService.addGreeting(user);
+	}
+
+	@GetMapping("/getGreeting/{id}")
+	public Optional<Greeting> getGreetingById(@PathVariable(value = "id") long id) {
+		return iGreetingService.getGreetingById(id);
+	}
+
+	@PostMapping("/post")
+	public Greeting getGreeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		user.setFirstName(name);
+		return iGreetingService.addGreeting(user);
+	}
 }
