ba06f69ea17de5eca43124b6eacfcfbf3bed47e5
Your Name <you@example.com>
2020-12-21 00:32:33 +0530

    Added: code to add the greeting

--- src/main/java/com/blz/greeting/controller/GreetingController.java
+++ src/main/java/com/blz/greeting/controller/GreetingController.java
@@ -1,12 +1,8 @@
 package com.blz.greeting.controller;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,31 +11,16 @@ import com.blz.greeting.model.User;
 import com.blz.greeting.service.IGreetingService;
 
 @RestController
+@RequestMapping("/greeting")
 public class GreetingController {
-	private static final String template = "Hello, %s!!";
-	private final AtomicLong counter = new AtomicLong();
 	
 	@Autowired
-	private IGreetingService greetingService;
-	
-	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(value="name", defaultValue="World")String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
-	}
-	
-	@GetMapping("/greeting/{name}")
-	public Greeting greetingNew(@PathVariable(value = "name") String name) {
-		return new Greeting(counter.incrementAndGet(),
-				String.format(template, name));
-	}
-	
-	@GetMapping("/message")
-	public String message() {
-		return greetingService.greet();
-	}
-	
-	@PostMapping("/fullName")
-	public String sayWow(@RequestBody User user) {
-		return "Hello "+user.firstName+" "+user.lastName;
-	}
+    private IGreetingService iGreetingService;
+
+    @GetMapping(value = {"/home"})
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    	User user = new User();
+        user.setFirstName(name);
+        return iGreetingService.addGreeting(user);
+    }
 }
